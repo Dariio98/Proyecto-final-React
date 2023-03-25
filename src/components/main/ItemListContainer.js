459f5d06c0026a765d8deeee6ab0cbe54eb26bfb
Dario Bristot <dariiob19@gmail.com>
2023-03-25 18:37:59 -0300

    nuevas imagenes

--- src/components/main/ItemListContainer.js
+++ src/components/main/ItemListContainer.js
@@ -9,8 +9,12 @@ const ItemListContainer = () => {
     return (
         <>
             <div className="greetingContainer">
-                <h2>{greeting}</h2>
-
+                <section className="hero">
+                    <div className="hero-container">
+                        <h2>{greeting}</h2>
+                        <p>Descubre nuestra amplia selección de productos para el hogar. Ofrecemos todo lo que necesitas para hacer que tu hogar sea cómodo, elegante y funcional. Desde muebles y accesorios hasta electrodomésticos y más, tenemos todo lo que necesitas para crear el hogar de tus sueños</p>
+                    </div>
+                </section>
                 <div className="containProducts">
                     <ItemList />
                 </div>
